969282d8a6167494e957028c4c8f1daaaacca63e
kazim487 <kazimraza13579@gmail.com>
2026-05-05 11:23:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function schoolName (firstSchool, lastSchool) {
+alert("first school"  + "last School")
+
+}
+schoolName()
+
+function teacherName (firstTeacher , lastTeacher) {
+alert("firstTeacher"  + "lastTeacher" )
+}
+teacherName()
+
+function brotherName (firstBrother , lastBrother) {
+alert("firstBrother" + "lastBrother")
+}
+brotherName()
+
+function collegeName (firstCollege , lastCollege) {
+alert("firstCollege" + "lastCollege")
+}
+collegeName()
+
+
+     
+
+   
